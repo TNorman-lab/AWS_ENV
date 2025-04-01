fb30a76e60198e7d265d3e0f6b95e60792da3f1a
TNorman-lab <taymwms@gmail.com>
2025-04-01 02:02:47 -0500

    added outputs and s3 file

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "my-terraform-prod-bucket"
+}
+
+variable "bucket_description" {
+  description = "A description for the S3 bucket"
+  type        = string
+  default     = "my terraform prod bucket"
+}
+
+variable "environment" {
+  description = "The deployment environment"
+  type        = string
+  default     = "production"
+}
